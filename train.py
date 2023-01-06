6f18b590bb9c13d2b0c3a6213f746d0a194f5beb
isLinXu <2267379130@qq.com>
2023-01-06 17:57:54 +0800

    1.update train.py

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ Train a YOLOv8 model on a custom dataset.
 Models and datasets download automatically from the latest YOLOv5 release.
 
 Usage - Single-GPU training:
-    $ python train.py --data coco128.yaml --weights yolov5s.pt --img 640  # from pretrained (recommended)
-    $ python train.py --data coco128.yaml --weights '' --cfg yolov5s.yaml --img 640  # from scratch
+    $ python train.py --data coco128.yaml --weights yolov8ns.pt --img 640  # from pretrained (recommended)
+    $ python train.py --data coco128.yaml --weights '' --cfg yolov8ns.yaml --img 640  # from scratch
 
 Usage - Multi-GPU DDP training:
     $ python -m torch.distributed.run --nproc_per_node 4 --master_port 1 train.py --data coco128.yaml --weights yolov5s.pt --img 640 --device 0,1,2,3
