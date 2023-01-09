91c9fd8b201b419427ffff6d0d866645ce5b6907
isLinXu <2267379130@qq.com>
2023-01-09 15:41:34 +0800

    1.fix val.py

--- val.py
+++ val.py
@@ -102,8 +102,8 @@ def run(
         weights=None,  # model.pt path(s)
         batch_size=32,  # batch size
         imgsz=640,  # inference size (pixels)
-        conf_thres=0.001,  # confidence threshold
-        iou_thres=0.6,  # NMS IoU threshold
+        conf=0.001,  # confidence threshold
+        iou=0.6,  # NMS IoU threshold
         max_det=300,  # maximum detections per image
         task='val',  # train, val, test, speed or study
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
@@ -142,8 +142,8 @@ def run(
 
         # Load model
         model = YOLO(weights)
-        model.val(data=data, save_json=True, save_hybrid=False, conf=0.001, iou=0.6, max_det=300, half=True,
-                  dnn=False, plots=False)
+        model.val(data=data, save_json=save_json, save_hybrid=save_hybrid, conf=conf, iou=iou, max_det=max_det, half=half,
+                  dnn=dnn, plots=plots)
 
         # Data
         data = check_dataset(data)  # check
@@ -160,145 +160,20 @@ def run(
     iouv = torch.linspace(0.5, 0.95, 10, device=device)  # iou vector for mAP@0.5:0.95
     niou = iouv.numel()
 
-    # Dataloader
-    # if not training:
-    #     if pt and not single_cls:  # check --weights are trained on --data
-    #         ncm = model.model.nc
-    #         assert ncm == nc, f'{weights} ({ncm} classes) trained on different --data than what you passed ({nc} ' \
-    #                           f'classes). Pass correct combination of --weights and --data that are trained together.'
-    #     model.warmup(imgsz=(1 if pt else batch_size, 3, imgsz, imgsz))  # warmup
-    #     pad, rect = (0.0, False) if task == 'speed' else (0.5, pt)  # square inference for benchmarks
-    #     task = task if task in ('train', 'val', 'test') else 'val'  # path to train/val/test images
-    #     dataloader = create_dataloader(data[task],
-    #                                    imgsz,
-    #                                    batch_size,
-    #                                    stride,
-    #                                    single_cls,
-    #                                    pad=pad,
-    #                                    rect=rect,
-    #                                    workers=workers,
-    #                                    prefix=colorstr(f'{task}: '))[0]
-    #
-    # seen = 0
-    confusion_matrix = ConfusionMatrix(nc=nc)
-    # names = model.names if hasattr(model, 'names') else model.module.names  # get class names
     names = data['names']
-    # if isinstance(names, (list, tuple)):  # old format
-    #     names = dict(enumerate(names))
-    # class_map = coco80_to_coco91_class() if is_coco else list(range(1000))
+
     s = ('%22s' + '%11s' * 6) % ('Class', 'Images', 'Instances', 'P', 'R', 'mAP50', 'mAP50-95')
     tp, fp, p, r, f1, mp, mr, map50, ap50, map = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
     # dt = Profile(), Profile(), Profile()  # profiling times
     loss = torch.zeros(3, device=device)
     jdict, stats, ap, ap_class = [], [], [], []
-    # callbacks.run('on_val_start')
-    # pbar = tqdm(dataloader, desc=s, bar_format=TQDM_BAR_FORMAT)  # progress bar
-    # for batch_i, (im, targets, paths, shapes) in enumerate(pbar):
-    #     callbacks.run('on_val_batch_start')
-    #     with dt[0]:
-    #         if cuda:
-    #             im = im.to(device, non_blocking=True)
-    #             targets = targets.to(device)
-    #         im = im.half() if half else im.float()  # uint8 to fp16/32
-    #         im /= 255  # 0 - 255 to 0.0 - 1.0
-    #         nb, _, height, width = im.shape  # batch size, channels, height, width
-    #
-    #     # Inference
-    #     with dt[1]:
-    #         preds, train_out = model(im) if compute_loss else (model(im, augment=augment), None)
-    #
-    #     # Loss
-    #     if compute_loss:
-    #         loss += compute_loss(train_out, targets)[1]  # box, obj, cls
-    #
-    #     # NMS
-    #     targets[:, 2:] *= torch.tensor((width, height, width, height), device=device)  # to pixels
-    #     lb = [targets[targets[:, 0] == i, 1:] for i in range(nb)] if save_hybrid else []  # for autolabelling
-    #     with dt[2]:
-    #         preds = non_max_suppression(preds,
-    #                                     conf_thres,
-    #                                     iou_thres,
-    #                                     labels=lb,
-    #                                     multi_label=True,
-    #                                     agnostic=single_cls,
-    #                                     max_det=max_det)
-    #
-        # # Metrics
-        # for si, pred in enumerate(preds):
-        #     labels = targets[targets[:, 0] == si, 1:]
-        #     nl, npr = labels.shape[0], pred.shape[0]  # number of labels, predictions
-        #     path, shape = Path(paths[si]), shapes[si][0]
-        #     correct = torch.zeros(npr, niou, dtype=torch.bool, device=device)  # init
-        #     seen += 1
-        #
-        #     if npr == 0:
-        #         if nl:
-        #             stats.append((correct, *torch.zeros((2, 0), device=device), labels[:, 0]))
-        #             if plots:
-        #                 confusion_matrix.process_batch(detections=None, labels=labels[:, 0])
-        #         continue
-    #
-    #         # Predictions
-    #         if single_cls:
-    #             pred[:, 5] = 0
-    #         predn = pred.clone()
-    #         scale_boxes(im[si].shape[1:], predn[:, :4], shape, shapes[si][1])  # native-space pred
-    #
-    #         # Evaluate
-    #         if nl:
-    #             tbox = xywh2xyxy(labels[:, 1:5])  # target boxes
-    #             scale_boxes(im[si].shape[1:], tbox, shape, shapes[si][1])  # native-space labels
-    #             labelsn = torch.cat((labels[:, 0:1], tbox), 1)  # native-space labels
-    #             correct = process_batch(predn, labelsn, iouv)
-    #             if plots:
-    #                 confusion_matrix.process_batch(predn, labelsn)
-    #         stats.append((correct, pred[:, 4], pred[:, 5], labels[:, 0]))  # (correct, conf, pcls, tcls)
-    #
-    #         # Save/log
-    #         if save_txt:
-    #             save_one_txt(predn, save_conf, shape, file=save_dir / 'labels' / f'{path.stem}.txt')
-    #         if save_json:
-                # save_one_json(predn, jdict, path, class_map)  # append to COCO-JSON dictionary
-    #         callbacks.run('on_val_image_end', pred, predn, path, names, im[si])
-    #
-    #     # Plot images
-    #     if plots and batch_i < 3:
-    #         plot_images(im, targets, paths, save_dir / f'val_batch{batch_i}_labels.jpg', names)  # labels
-    #         plot_images(im, output_to_target(preds), paths, save_dir / f'val_batch{batch_i}_pred.jpg', names)  # pred
-    #
-    #     callbacks.run('on_val_batch_end', batch_i, im, targets, paths, shapes, preds)
-    #
+
     # Compute metrics
     stats = [torch.cat(x, 0).cpu().numpy() for x in zip(*stats)]  # to numpy
     if len(stats) and stats[0].any():
         tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats, plot=plots, save_dir=save_dir, names=names)
         ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
         mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
-    # nt = np.bincount(stats[3].astype(int), minlength=nc)  # number of targets per class
-
-    # # Print results
-    # pf = '%22s' + '%11i' * 2 + '%11.3g' * 4  # print format
-    # LOGGER.info(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
-    # if nt.sum() == 0:
-    #     LOGGER.warning(f'WARNING ⚠️ no labels found in {task} set, can not compute metrics without labels')
-    #
-    # # Print results per class
-    # if (verbose or (nc < 50 and not training)) and nc > 1 and len(stats):
-    #     for i, c in enumerate(ap_class):
-    #         LOGGER.info(pf % (names[c], seen, nt[c], p[i], r[i], ap50[i], ap[i]))
-    #
-    # # Print speeds
-    # t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
-    # if not training:
-    #     shape = (batch_size, 3, imgsz, imgsz)
-    #     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {shape}' % t)
-    #
-    # Plots
-    # if plots:
-    #     ConfusionMatrix.plot(save_dir=save_dir, names=list(names.values()))
-    #     callbacks.run('on_val_end', nt, tp, fp, p, r, f1, ap, ap50, ap_class, confusion_matrix)
-    #
-    # print('jdict:', jdict)
 
     # # Save JSON
     if save_json and len(jdict):
@@ -343,11 +218,22 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'weights/yolov8n.pt', help='model path(s)')
+    parser.add_argument('--save-json', default= True, action='store_true', help='save a COCO-JSON results file')
+    parser.add_argument('--save-hybrid', default= False, action='store_true', help='save label+prediction hybrid results to *.txt')
+    parser.add_argument('--conf', type=float, default=0.001, help='confidence threshold')
+    parser.add_argument('--iou', type=float, default=0.6, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=300, help='maximum detections per image')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--plots', default= True, action='store_true', help='')
+
+
+
+
+
+
     parser.add_argument('--batch-size', type=int, default=32, help='batch size')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.001, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.6, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=300, help='maximum detections per image')
     parser.add_argument('--task', default='val', help='train, val, test, speed or study')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--workers', type=int, default=8, help='max dataloader workers (per RANK in DDP mode)')
@@ -355,14 +241,10 @@ def parse_opt():
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--verbose', action='store_true', help='report mAP by class')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-hybrid', action='store_true', help='save label+prediction hybrid results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-json', action='store_true', help='save a COCO-JSON results file')
     parser.add_argument('--project', default=ROOT / 'runs/val', help='save to project/name')
     parser.add_argument('--name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
     opt = parser.parse_args()
     opt.data = check_yaml(opt.data)  # check YAML
     opt.save_json |= opt.data.endswith('coco.yaml')
@@ -375,7 +257,7 @@ def main(opt):
     check_requirements(exclude=('tensorboard', 'thop'))
 
     if opt.task in ('train', 'val', 'test'):  # run normally
-        if opt.conf_thres > 0.001:  # https://github.com/ultralytics/yolov5/issues/1466
+        if opt.conf > 0.001:  # https://github.com/ultralytics/yolov5/issues/1466
             LOGGER.info(f'WARNING ⚠️ confidence threshold {opt.conf_thres} > 0.001 produces invalid results')
         if opt.save_hybrid:
             LOGGER.info('WARNING ⚠️ --save-hybrid will return high mAP from hybrid labels, not from predictions alone')
