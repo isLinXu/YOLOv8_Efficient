2e87839afc8eb62c46753b922b26301a35b39a3c
isLinXu <2267379130@qq.com>
2023-01-11 13:57:32 +0800

    1.update metrics_plots.py

--- utils/metrics_plots.py
+++ utils/metrics_plots.py
@@ -1,5 +1,18 @@
-import matplotlib.pyplot as plt
+
 import math
+import matplotlib.pyplot as plt
+
+
+
+def is_nan(x):
+    return type(x) is float and math.isnan(float(x))
+
+import pandas
+def pd_read_csv(csv_path):
+
+    df = pandas.read_csv(csv_path)
+    # print(df)
+    return df
 
 def fps_to_ms(fps: int) -> int:
     '''
@@ -12,86 +25,45 @@ def fps_to_ms(fps: int) -> int:
     return math.floor((1 / fps) * 1000)
 
 
-def plot_mult_metrics():
-    '''
-        Draw Metrics plots
-        Args:
+def plot_metrics(df,fig_name):
+    model_list = df['model'].unique()
+    # print('model_list:',model_list)
+    font_size = 5
 
-        Returns:
+    for i in range(0, len(model_list)):
+        label_list = df[df['model'] == model_list[i]]['branch'].tolist()
+        ms_list = df[df['model'] == model_list[i]]['ms'].values
+        fps_list = df[df['model'] == model_list[i]]['fps'].values
+        map_list = df[df['model'] == model_list[i]]['mAP'].values
+        # print('label_list', label_list, 'ms',ms_list, 'fps', fps_list, 'map', map_list)
+        y_list = map_list
+        t_list = []
+        # print('fps_list[0]', fps_list[0])
 
-        '''
-    # yolov5
-    x1_1 = [6.3, 6.4, 8.2, 10.1, 12.1]
-    y1_1 = [28.0, 37.4, 45.4, 49.0, 50.7]
-    l1_1 = ['YOLOv5n', 'YOLOv5s', 'YOLOv5m', 'YOLOv5l', 'YOLOv5x']
+        if fps_list[0] == -1:
+            x_list = ms_list
+        else:
+            for j in fps_list:
+                j = fps_to_ms(j)
+                t_list.append(j)
+            x_list = t_list
 
-    ###############################################
-    # yolov6
-    # 1fps=0.3048 m/s
-    x2_1 = [fps_to_ms(779), fps_to_ms(339), fps_to_ms(175), fps_to_ms(98)]
-    y2_1 = [37.5, 45.0, 50.0, 52.8]
-    l2_1 = ['YOLOv6-N', 'YOLOv6-S', 'YOLOv6-M', 'YOLOv6-L']
-
-    x2_2 = [fps_to_ms(228), fps_to_ms(98), fps_to_ms(47), fps_to_ms(26)]
-    y2_2 = [44.9, 50.3, 55.2, 57.2]
-    l2_2 = ['YOLOv6-N6', 'YOLOv6-S6', 'YOLOv6-M6', 'YOLOv6-L6']
-    ###############################################
-
-    # yolov7
-    x3 = [fps_to_ms(161), fps_to_ms(114), fps_to_ms(84), fps_to_ms(56), fps_to_ms(44), fps_to_ms(36)]
-    y3 = [51.4, 53.1, 54.9, 56.0, 56.6, 56.8]
-    l3 = ['YOLOv7', 'YOLOv7-X', 'YOLOv7-W6', 'YOLOv7-E6', 'YOLOv7-D6', 'YOLOv7-E6E']
-
-    ###############################################
-    # yolov8
-    x4_1 = [5.6, 5.7, 8.3, 13.1, 20.4]
-    y4_1 = [37.3, 44.9, 50.2, 52.9, 53.9]
-    l4_1 = ['YOLOv8n', 'YOLOv8s', 'YOLOv8m', 'YOLOv8l', 'YOLOv8x']
-
-    x4_2 = [11.3, 11.4, 15.3, 16.8, 23.8]
-    y4_2 = [30.7, 37.0, 40.6, 42.5, 43.2]
-    l4_2 = ['YOLOv8n-seg', 'YOLOv8s-seg', 'YOLOv8m-seg', 'YOLOv8l-seg', 'YOLOv8x-seg']
-
-    ###############################################
-    # plot
-    # yolov5
-    plt.plot(x1_1, y1_1, marker='.', markersize=10)
-
-    # yolov6
-    plt.plot(x2_1, y2_1, marker='.', markersize=10)
-    plt.plot(x2_2, y2_2, marker='.', markersize=10)
-
-    # yolov7
-    plt.plot(x3, y3, marker='.', markersize=10)
-
-    # yolov8
-    plt.plot(x4_1, y4_1, marker='.', markersize=10)
-    plt.plot(x4_2, y4_2, marker='.', markersize=10)
-
-    plt.title("yolov8 metrics")
-    plt.xlabel('PyTorch FP16 RTX3080(ms/img)')  # x轴标题
-    plt.ylabel('COCO Mask AP val')  # y轴标题
-
-    for fps, map, label in zip(x1_1, y1_1, l1_1):
-        plt.text(fps, map, label, ha='center', va='bottom', fontsize=6)
-    for fps, map, label in zip(x2_1, y2_1, l2_1):
-        plt.text(fps, map, label, ha='center', va='bottom', fontsize=6)
-    for fps, map, label in zip(x2_2, y2_2, l2_2):
-        plt.text(fps, map, label, ha='center', va='bottom', fontsize=6)
-    for fps, map, label in zip(x3, y3, l3):
-        plt.text(fps, map, label, ha='center', va='bottom', fontsize=6)
-    for fps, map, label in zip(x4_1, y4_1, l4_1):
-        plt.text(fps, map, label, ha='center', va='bottom', fontsize=6)
-    for fps, map, label in zip(x4_2, y4_2, l4_2):
-        plt.text(fps, map, label, ha='center', va='bottom', fontsize=6)
-
+        plt.plot(x_list, y_list, marker='.', markersize=10)
+        plt.title("yolov8 metrics")
+        plt.xlabel('PyTorch FP16 RTX3080(ms/img)')  # x轴标题
+        plt.ylabel('COCO Mask AP val')  # y轴标题
+        for ms, map, label in zip(x_list, y_list, label_list):
+            plt.text(ms, map, label, ha='center', va='bottom', fontsize=font_size)
     # legend
-    plt.legend(['YOLOv5', 'YOLOv6', 'YOLOv6-6', 'YOLOv7', 'YOLOv8', 'YOLOv8-seg'], loc='lower right')
+    plt.legend(model_list, loc='lower right')
 
     # save
-    plt.savefig("plot_metrics.jpg",dpi=640)
+    plt.savefig(fig_name, dpi=640)
     # show
     plt.show()
 
 if __name__ == '__main__':
-    plot_mult_metrics()
+    csv_path = '/home/linxu/PycharmProjects/Yolov8_Efficient/log/yolo_model_data.csv'
+    fig_name = 'plot_metrics.jpg'
+    df = pd_read_csv(csv_path)
+    plot_metrics(df, fig_name)
